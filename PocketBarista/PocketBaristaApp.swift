1b2f3e3c3b7c8bf151bf09784af8b4f84f723314
mike-griffin <mike_griffin@comcast.net>
2021-06-21 20:14:29 -0700

    Initial Commit

---
+++ PocketBarista/PocketBaristaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketBaristaApp.swift
+//  PocketBarista
+//
+//  Created by Mike Griffin on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct PocketBaristaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
