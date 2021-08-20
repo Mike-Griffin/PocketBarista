5b757fcff9a70f21ff9aa55ea9dcb139bedf7daa
mike-griffin <mike_griffin@comcast.net>
2021-08-20 16:57:08 -0700

    Finished cleaning up the brew log detail view

---
+++ PocketBarista/Utilities/AlertItem.swift
@@ -0,0 +1,8 @@
+//
+//  AlertItem.swift
+//  PocketBarista
+//
+//  Created by Mike Griffin on 8/20/21.
+//
+
+import Foundation
---
+++ PocketBarista/Views/Screens/BrewLogDetailView/BrewLogDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  BrewLogDetailView.swift
+//  PocketBarista
+//
+//  Created by Mike Griffin on 8/20/21.
+//
+
+import SwiftUI
+
+struct BrewLogDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BrewLogDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrewLogDetailView()
+    }
+}
